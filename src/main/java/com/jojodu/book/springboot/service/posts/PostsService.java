1d84f88583d42a25de1a7e41e1639ec940aa24c8
pozafly <pozafly@gmail.com>
2020-05-29 17:53:54 +0900

    second commit

--- src/main/java/com/jojodu/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jojodu/book/springboot/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.jojodu.book.springboot.service.posts;
 
 import com.jojodu.book.springboot.domain.posts.Posts;
 import com.jojodu.book.springboot.domain.posts.PostsRepository;
+import com.jojodu.book.springboot.web.dto.PostsListResponseDto;
 import com.jojodu.book.springboot.web.dto.PostsResponseDto;
 import com.jojodu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jojodu.book.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,10 +10,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 @Service
 public class PostsService {
-
     private final PostsRepository postsRepository;
 
     @Transactional
@@ -35,6 +38,14 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc()
+                .stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 
 }
--- src/main/java/com/jojodu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojodu/book/springboot/web/IndexController.java
@@ -1,10 +1,22 @@
 package com.jojodu.book.springboot.web;
 
+import com.jojodu.book.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final PostsService postsService;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        return "index";
+    }
 
     @GetMapping
     public String index() {
@@ -15,4 +27,5 @@ public class IndexController {
     public String postsSave() {
         return "posts-save";
     }
+
 }
