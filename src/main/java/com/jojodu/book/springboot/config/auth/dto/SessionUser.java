71a5b8357b8b2ea2e35e717f10510e0e346465f5
pozafly <pozafly@gmail.com>
2020-06-02 13:49:03 +0900

    OAuth2 config complete

--- src/main/java/com/jojodu/book/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/jojodu/book/springboot/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package com.jojodu.book.springboot.config.auth.dto;
 import com.jojodu.book.springboot.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
